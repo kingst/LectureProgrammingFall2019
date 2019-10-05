d556a17c8e047621e5f29844f3d8a217b9d8d9c3
Sam King <kingst@gmail.com>
2019-10-05 08:15:09 -0700

    After 10/3 lecture

--- TimerApp/TimerApp/ViewController.swift
+++ TimerApp/TimerApp/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startStopButton: UIButton!
+    var lapCount: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startStopButtonPress() {
+        print("startStop press")
+        self.toggleButton(between: ("Start", "Stop"), on: self.startStopButton)
+        //self.dontUse("hello")
+        //self.useIfYouWant(foo: "bar")
+    }
+    
+    func toggleButton(between titles: (String, String), on button: UIButton) {
+        let title = button.currentTitle == titles.0 ? titles.1 : titles.0
+        button.setTitle(title, for: .normal)
+        
+    }
+    
+    /*
+    func dontUse(_ foo: String) {
+        
+    }*/
+    
+    /*
+    func useIfYouWant(foo: String) {
+        
+    }*/
+    @IBAction func lapButtonPressed() {
+        self.lapCount += 1
+        print("Lap button pressed \(self.lapCount)")
+    }
 }
 
