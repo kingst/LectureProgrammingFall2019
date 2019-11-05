60ab04401210eb234d2fab7cee129d2b4ba8fc74
Sam King <kingst@gmail.com>
2019-11-05 09:17:48 -0800

    Starter code

--- webserver.playground/Contents.swift
+++ webserver.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+func newClientConnection(serverSocket: Int, connected: ((_ socket: Int?, _ error: Error?) -> Void)) {
+    
+}
+
+func readHTTPRequest(socket: Int, complete: ((_ request: String?, _ error: Error?) -> Void)) {
+    
+}
+
+func accessFilesystem(request: String, complete: ((_ fileSystem: String?, _ error: Error?) -> Void)) {
+    
+}
+
+func accessDatabase(request: String, complete: ((_ database: String?, _ error: Error?) -> Void)) {
+    
+}
+
+func sendResponse(socket: Int, request: String, database: String, filesystem: String) {
+    
+}
+
+
+// =========================================
+
